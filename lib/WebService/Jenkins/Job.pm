d7d991b8f1eef0d4e09c56baf9b73e8d4afa4c6a
Adam Taylor <adamjctaylor@gmail.com>
2011-10-18 09:18:28 +0100

    Add additional builder methods

--- lib/WebService/Jenkins/Job.pm
+++ lib/WebService/Jenkins/Job.pm
@@ -7,6 +7,9 @@ with 'WebService::Jenkins::Role::Client'
 use Carp qw/ croak /;
 use Storable;
 
+use WebService::Jenkins::Build;
+use WebService::Jenkins::Build::HealthReport;
+
 has 'data' => (
     isa => 'HashRef',
     is => 'ro',
@@ -81,42 +84,131 @@ has 'first_build' => (
     isa => 'WebService::Jenkins::Build',
     is => 'rw',
 );
+sub _build_first_build {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build->new({
+        number => $self->data->{firstBuild}->{number},
+        url => $self->data->{firstBuild}->{url},
+    });
+}
+
 has 'health_report' => (
     isa => 'Str',
     is => 'rw',
 );
+sub _build_health_report {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build::HealthReport->new({
+        description => $self->data->{healthReport}->{description},
+        icon_url => $self->data->{healthReport}->{iconUrl},
+        score => $self->data->{healthReport}->{score},
+    });
+}
+
 has in_queue => (
     isa => 'Bool',
     is => 'rw',
 );
+sub _build_in_queue {
+    my ( $self ) = @_;
+
+    return $self->bool( $self->data->{inQueue} );
+}
+
 has 'last_build' => (
     isa => 'WebService::Jenkins::Build',
     is => 'rw',
 );
+sub _build_last_build {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build->new({
+        number => $self->data->{lastBuild}->{number},
+        url => $self->data->{lastBuild}->{url},
+    });
+}
+
 has 'last_completed_build' => (
     isa => 'WebService::Jenkins::Build',
     is => 'rw',
 );
+sub _build_last_completed_build {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build->new({
+        number => $self->data->{lastCompletedBuild}->{number},
+        url => $self->data->{lastCompletedBuild}->{url},
+    });
+
+}
+
 has 'last_failed_build' => (
     isa => 'WebService::Jenkins::Build',
     is => 'rw',
 );
+sub _build_last_failed_build {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build->new({
+        number => $self->data->{lastFailedBuild}->{number},
+        url => $self->data->{lastFailedBuild}->{url},
+    });
+
+}
+
 has 'last_stable_build' => (
     isa => 'WebService::Jenkins::Build',
     is => 'rw',
 );
+sub _build_last_stable_build {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build->new({
+        number => $self->data->{lastStableBuild}->{number},
+        url => $self->data->{lastStableBuild}->{url},
+    });
+
+}
+
 has 'last_unstable_build' => (
     isa => 'WebService::Jenkins::Build',
     is => 'rw',
 );
+sub _build_last_unstable_build {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build->new({
+        number => $self->data->{lastUnstableBuild}->{number},
+        url => $self->data->{lastUnstableBuild}->{url},
+    });
+
+}
+
 has 'last_unsuccessful_build' => (
     isa => 'WebService::Jenkins::Build',
     is => 'rw',
 );
+sub _build_last_unsuccessful_build {
+    my ( $self ) = @_;
+
+    return WebService::Jenkins::Build->new({
+        number => $self->data->{lastUnsuccessfulBuild}->{number},
+        url => $self->data->{lastUnsuccessfulBuild}->{url},
+    });
+
+}
+
 has 'next_build_number' => (
     isa => 'Int',
     is => 'rw',
 );
+sub _build_next_build_number {
+    my ( $self ) = @_;
+
+    return $self->data->{nextBuildNumber};
+}
 
 # Intercept massive Moose stacktraces and throw more human friendly errors.
 # Uneccesary?
